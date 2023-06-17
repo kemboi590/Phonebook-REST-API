e639d859e06d2a2d5383452a181438f656aaa3d6
kemboi590 <bkemboi590@gmail.com>
2023-06-17 13:32:41 +0300

    phonebook api

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import config from "./src/db/config.js";
+import bodyParser from "body-parser";
+import user from "./src/routes/routes.js";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// JWT middleware
+app.use((req, res, next) => {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "JWT"
+  ) {
+    jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      config.jwt_secret,
+      (err, decode) => {
+        if (err) req.user = undefined;
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+
+user(app);
+
+app.listen(config.port, () => {
+  console.log(`Server running at ${config.url}`);
+});
